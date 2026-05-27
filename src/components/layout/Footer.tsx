55833ad2d674d8ee4bfd2d3267ae4c4ab7a495bb
Lienkulet <clocicovalexandru@gmail.com>
2026-05-27 22:55:10 +0300

    footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,9 +1,86 @@
-import React from 'react'
-
+import Link from 'next/link';
+import Container from './Container';
 const Footer = () => {
+
   return (
-    <div>Footer</div>
-  )
+    <footer
+      className="
+        border-t
+        border-[oklch(0.11_0.04_310)]
+        bg-[oklch(0.04_0.025_305)]
+        py-7
+      "
+    >
+      <Container
+        className="
+          flex
+          flex-col
+          items-center
+          justify-between
+          gap-4.5
+          text-center
+          lg:flex-row
+        "
+      >
+        <Link
+          href="#"
+          className="
+            text-[13px]
+            font-bold
+            tracking-[0.14em]
+            text-(--text-muted)
+            font-cinzel-decorative
+          "
+        >
+          OUTFIT
+          <span className="text-(--magenta-dim)">
+            &nbsp;SANTO
+          </span>
+        </Link>
+
+        <span
+          className="
+            text-[9px]
+            uppercase
+            tracking-[0.2em]
+            text-[oklch(0.28_0.03_305)]
+            font-cinzel
+          "
+        >
+          © 2026 Outfit Santo. All rights reserved.
+        </span>
+
+        <ul
+          className="
+            flex
+            list-none
+            items-center
+            gap-7
+          "
+        >
+          {['Privacy', 'Terms', 'Contact', 'Instagram'].map((item) => (
+            <li key={item}>
+              <Link
+                href="#"
+                className="
+                  text-[9px]
+                  uppercase
+                  tracking-[0.2em]
+                  text-[oklch(0.34_0.04_310)]
+                  transition-colors
+                  duration-300
+                  hover:text-(--magenta)
+                  font-cinzel
+                "
+              >
+                {item}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </Container>
+    </footer>
+  );
 }
 
 export default Footer
